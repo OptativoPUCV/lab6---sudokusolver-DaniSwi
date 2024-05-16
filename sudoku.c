803c84036d086d607af6d6e9af3eba64da196199
Your Name <you@example.com>
2024-05-16 21:49:05 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -95,7 +95,7 @@ Node *DFS(Node *initial, int *cont) {
   Stack *S = createStack();
   push(S, initial);
   while (get_size(S) != 0){
-    Node *n = top(S);
+    Node *n = pop(S);
     pop(S);
     if(is_final(n))
       return n;
