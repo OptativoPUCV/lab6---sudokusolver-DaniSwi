9c68522aa1a2a06c50beadf02e4516d4ba6b474e
Your Name <you@example.com>
2024-05-16 21:46:34 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -70,9 +70,8 @@ List *get_adj_nodes(Node *n) {
         for (k = 1; k <= 9; ++k) {
           Node *new = copy(n);
           new->sudo[i][j] = k;
-          pushBack(list, new);
-          if(is_valid(new) == 0)
-            popBack(list);
+          if(is_valid(new))
+            pushFront(list, new);
         }
       }
     }
